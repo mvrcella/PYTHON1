e46e2837f90f5aef4980b7f38e09c26e0ec833ad
mars <82300924+mvrcella@users.noreply.github.com>
2023-06-07 14:03:38 -0300

    Add files via upload

---
+++ operadoresrelacionais.py
@@ -0,0 +1,21 @@
+print()
+n1 = int(input('Informe um número: '))
+n2 = int(input('Informe outro número: '))
+
+print(n1, '==', n2, '->', n1 == n2) 
+# /\ Informa se os números são iguais
+
+print(n1, '!=', n2, '->', n1 != n2)
+# /\ Informa se os números são diferentes
+
+print(n1, '<', n2, '->', n1 < n2)
+# /\ Informa se o primeiro número é menor que o segundo
+
+print(n1, '<=', n2, '->', n1 <= n2)
+# /\ Informa se o primeiro número é menor ou igual ao segundo 
+
+print(n1, '>', n2, '->', n1 > n2)
+# /\ Informa se o primeiro número é maior que o segundo
+
+print(n1, '>=', n2, '->', n1 >= n2)
+# /\ Informa se o primeiro número é maior ou igual ao segundo
