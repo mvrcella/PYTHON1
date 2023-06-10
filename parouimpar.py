ca7ca661ba91b9a45bb25deccc493bb8da2a3a3d
mars <82300924+mvrcella@users.noreply.github.com>
2023-06-10 18:37:10 -0300

    Add files via upload

---
+++ parouimpar.py
@@ -0,0 +1,7 @@
+num = float(input('Informe o número que será classificado em par ou ímpar: '))
+resto = num % 2
+if resto == 0:
+   print('O número informado é par') 
+if not resto ==0:
+    print ('O número informado é ímpar')
+
